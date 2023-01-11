a9d2b9cc709b09c5269048c50e01f00936187134
Dave Morgan <dave@dmorg.org>
2023-01-11 16:37:07 -0700

    routes

---
+++ src/libs/components/Header.svelte
@@ -0,0 +1,61 @@
+<script>
+	export let topMatter;
+	$: tm = topMatter;
+</script>
+
+<header>
+	<!-- {#if tm.course === 'home'} -->
+		<div class="left">
+			<h3>{tm.description} :: </h3>
+			<h3>{tm.courseModule} </h3>
+		</div>
+	<!-- {:else} -->
+		<!-- <h3><a href="/">{tm.description}</a></h3>
+		<h3><a href="/{tm.course}">{tm.course}</a></h3> -->
+	<!-- {/if} -->
+	<!-- <div class="left">
+		<h3><a href="/">{topMatter.description} :: </a></h3>
+		{#if topMatter.course == 'home'}
+			<h3>{topMatter.courseModule}</h3>
+		{:else}
+			<h3><a href="/{topMatter.course}">{topMatter.course}</a></h3>
+		{/if}
+	</div>
+	{#if topMatter.course == 'home'}
+		<h3 class="classModule">Contents</h3>
+	{:else}
+		<h3 class="classModule">{topMatter.courseModule}</h3>
+	{/if} -->
+</header>
+
+{topMatter.description}:{topMatter.course}:{topMatter.courseModule}
+
+<style>
+	header {
+		background: var(--main-grey);
+		box-shadow: -0.25px 5px 15px 0px rgba(0, 0, 0, 0.5);
+		display: flex;
+		font-size: 150%;
+		justify-content: space-between;
+		margin-bottom: 1em;
+		padding: 1rem 2rem;
+		text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.75);
+		color: var(--dark-grey);
+	}
+	h3 {
+		display: inline-block;
+		text-transform: capitalize;
+		letter-spacing: 0.05rem;
+		margin: 0;
+	}
+	/* a {
+		color: inherit;
+		text-decoration: none;
+	} */
+	.left {
+		margin-right: 2em;
+	}
+	.classModule {
+		text-align: right;
+	}
+</style>
